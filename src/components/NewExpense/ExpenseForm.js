67a24fbe87b13e52718b45e5cfac7f9748beff94
Tuyen Pham <tuh39709@temple.edu>
2021-09-27 14:29:26 -0400

    add filter year for every expense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ export default function ExpenseForm(props) {
 
         const expenseData = {
             title: enteredTitle,
-            amount: setEnteredAmount,
+            amount: enteredAmount,
             date: new Date(enteredDate)
         }
 
